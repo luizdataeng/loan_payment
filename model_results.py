ec4bda02aaf5e46f473fc7b8d509c7b2eadf1688
luizsaraivat <103157907+luizsaraivat@users.noreply.github.com>
2025-04-23 23:12:13 -0400

    comparison between models

---
+++ model_results.py
@@ -0,0 +1,47 @@
+results_dict = {
+    'Logistic Regression': {
+        'Accuracy': 0.894,
+        'Precision': 0.782,
+        'Recall': 0.727,
+        'F1-Score': 0.753,
+        'ROC-AUC': 0.951,
+        'Log Loss': 0.228
+    },
+    'KNN': {
+        'Accuracy': 0.889,
+        'Precision': 0.795,
+        'Recall': 0.672,
+        'F1-Score': 0.728,
+        'ROC-AUC': 0.917,
+        'Log Loss': 0.875
+    },
+    'Support Vector Regression': {
+        'MSE': 0.154,
+        'MAE': 0.248,
+        'R² Score': 0.111
+    },
+    'Decision Tree': {
+        'Accuracy': 0.893,
+        'Precision': 0.751,
+        'Recall': 0.777,
+        'F1-Score': 0.763,
+        'ROC-AUC': 0.851,
+        'Log Loss': 3.856
+    },
+    'Random Forest': {
+        'Accuracy': 0.926,
+        'Precision': 0.898,
+        'Recall': 0.754,
+        'F1-Score': 0.820,
+        'ROC-AUC': 0.971,
+        'Log Loss': 0.180
+    },
+    'XGBoost': {
+        'Accuracy': 0.917,
+        'Precision': 0.869,
+        'Recall': 0.740,
+        'F1-Score': 0.799,
+        'ROC-AUC': 0.968,
+        'Log Loss': 0.183
+    }
+}
